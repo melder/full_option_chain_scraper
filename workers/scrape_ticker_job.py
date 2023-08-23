d00485aadf3de2991d2dbb7e2f34a068680a15f4
melder <eldertree@gmail.com>
2023-08-23 01:38:45 -0700

    forgot to enable blacklist

--- workers/scrape_ticker_job.py
+++ workers/scrape_ticker_job.py
@@ -1,6 +1,9 @@
 from config import config
 from scraper import IvScraper
+from models.blacklist import Blacklist
 
 
 def scrape_ticker_job(ticker, timestamp):
-    IvScraper(ticker, None, timestamp, config.mongo_client()).scrape()
+    iv_scraper = IvScraper(ticker, None, timestamp, config.mongo_client())
+    iv_scraper.scrape()
+    Blacklist(iv_scraper).exec()
