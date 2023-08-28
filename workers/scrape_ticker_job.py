2c0fd2366e74b7b5254857e273e2131288495a9a
melder <eldertree@gmail.com>
2023-08-28 10:30:07 -0700

    fixes scrape job

--- workers/scrape_ticker_job.py
+++ workers/scrape_ticker_job.py
@@ -4,6 +4,8 @@ from models.blacklist import Blacklist
 
 
 def scrape_ticker_job(ticker, timestamp):
-    iv_scraper = IvScraper(ticker, None, timestamp, config.mongo_client())
+    mongo_client = config.mongo_client()
+    iv_scraper = IvScraper(ticker, None, timestamp, mongo_client)
     iv_scraper.scrape()
+    mongo_client.close()
     Blacklist(iv_scraper).exec()
